32292c51ebece6880b17444e7767ccc4879a6c71
jaseel <jaseeljctcs@gmail.com>
2022-11-27 23:14:04 +0530

    init search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,32 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FriendsAppAddComponent } from './friends-app-add/friends-app-add.component';
 import { FriendAppViewComponent } from './friend-app-view/friend-app-view.component';
-
+import { FriendAppSearchComponent } from './friend-app-search/friend-app-search.component';
+import { RouterModule,Routes } from '@angular/router';
+const myroute:Routes=[
+  {
+    path:'',
+    'component':FriendsAppAddComponent
+  },
+  {
+    path:'view',
+    'component':FriendAppViewComponent
+  },{
+    path:'search',
+    'component':FriendAppSearchComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
     FriendsAppAddComponent,
-    FriendAppViewComponent
+    FriendAppViewComponent,
+    FriendAppSearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myroute)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/friend-app-search/friend-app-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-friend-app-search',
+  templateUrl: './friend-app-search.component.html',
+  styleUrls: ['./friend-app-search.component.css']
+})
+export class FriendAppSearchComponent {
+
+}
